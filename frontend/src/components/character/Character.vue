dc7808607a63ab4a43fc46c0cf79b9dac54ebba4
stras <stras1914@163.com>
2026-06-21 22:41:39 +0800

    实现好友列表

--- frontend/src/components/character/Character.vue
+++ frontend/src/components/character/Character.vue
@@ -8,7 +8,9 @@ import RemoveIcon from "@/components/icon/RemoveIcon.vue";
 import {useRouter} from "vue-router";
 import ChatField from "@/components/character/chat_field/ChatField.vue";
 
-const props = defineProps(['character', 'canEdit'])
+
+const props = defineProps(['character', 'canEdit', 'canRemoveFriend', 'friendId'])
+
 const emit = defineEmits(['remove'])
 const isHover = ref(false)
 const user = useUserStore()
@@ -50,6 +52,18 @@ async function openChatField() {
   }
 }
 
+async function handleRemoveFriend() {
+  try {
+    const res = await api.post('api/friend/remove', {
+      friend_id: props.friendId,
+    })
+    if (res.data.result === 'success') {
+      emit('remove', props.friendId)
+    }
+  } catch (err) {
+  }
+}
+
 </script>
 
 <template>
@@ -60,13 +74,20 @@ async function openChatField() {
         <div class="absolute left-0 top-50 w-60 h-50 bg-linear-to-t from-black/40 to-transparent"></div>
 
         <div v-if="canEdit && character.author.user_id === user.id" class="absolute right-0 top-67">
-          <RouterLink :to="{name: 'update-character', params: {character_id: character.id}}" class="btn btn-circle btn-ghost bg-transparent">
+          <RouterLink @click.stop :to="{name: 'update-character', params: {character_id: character.id}}" class="btn btn-circle btn-ghost bg-transparent">
             <UpdateIcon />
           </RouterLink>
-          <button @click="handleRemoveCharacter" class="btn btn-circle btn-ghost bg-transparent">
+          <button @click.stop="handleRemoveCharacter" class="btn btn-circle btn-ghost bg-transparent">
             <RemoveIcon />
           </button>
         </div>
+
+        <div v-if="canRemoveFriend" class="absolute right-0 top-50">
+          <button @click.stop="handleRemoveFriend" class="btn btn-circle btn-ghost bg-transparent">
+            <RemoveIcon />
+          </button>
+        </div>
+
 
         <div class="absolute left-4 top-60 avatar">
           <div class="w-16 rounded-full ring-3 ring-white">
